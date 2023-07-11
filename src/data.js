017e5e79302a4d1500f0e4149719ab6e8e08193a
Shinjin Santhakumar <shinjinoliver@gmail.com>
2023-07-10 20:55:40 -0700

    Deploy

---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const projects = [
+  {
+    title: "Frozen Frontier",
+    subtitle: "Unity, C#, Game development",
+    description:
+      "I worked with a team of 3 to create a game in 72 hours that placed 20th in over 200 entries. Focused around the theme of 10 seconds",
+    image: "./project-1.gif",
+    link: "https://thegreenphoenix.itch.io/ff",
+  },
+  {
+    title: "Client Server App",
+    subtitle: "Python, Flask, Poltly",
+    description:
+      "Created ",
+    image: "./project-2.gif",
+    link: "https://github.com/shinjin-santhakumar/Client-Server-App",
+  },
+  {
+    title: "Sketch",
+    subtitle: "Unity, C#, Game development",
+    description:
+      "Co-created a game at the UCR game development club, Gamespawn, where we worked on the project for 2 quraters and eventually published the game to the popular video game distribution service, Steam.",
+    image: "./project-3.gif",
+    link: "https://store.steampowered.com/app/1943220/Sketch/",
+  },
+  {
+    title: "Terminal Chess Game",
+    subtitle: "C++, S.O.L.I.D. Design principles",
+    description:
+      "Created and implemented chess using the default windows terminal. Made use of common design patterns and principles",
+    image: "./InitialPrompt.PNG",
+    link: "https://github.com/shinjin-santhakumar/Chess-Game",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "C++",
+  "Python",
+  "C#",
+  "Java",
+  "HTML/CSS",
+  "React",
+  "Bootstrap",
+  ".NET",
+  "Git",
+  "ASP .NET",
+  "SQL"
+];
