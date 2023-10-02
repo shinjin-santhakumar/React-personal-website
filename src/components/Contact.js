b8556e0705a42a386e2fd7bac4fd043501982664
Shinjin Santhakumar <shinjinoliver@gmail.com>
2023-10-01 19:28:22 -0700

    contact through formsubmit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,9 +63,9 @@ export default function Contact() {
           </div>
         </div>
         <form
-          netlify
+          action="https://formsubmit.co/09b756c324308de62e72d8666a21c49f" 
+          method="POST"
           name="contact"
-          onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me
